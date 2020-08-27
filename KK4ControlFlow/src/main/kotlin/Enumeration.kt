2131855b8c2b14739c57be0365df3aefc18613a3
Sectio <ramadhani2910712@gmail.com>
2020-08-27 20:51:54 +0700

    first commit

---
+++ KK4ControlFlow/src/main/kotlin/Enumeration.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val getPlayerLevel = (playerID.Sectio.value)
+    print("Player Level is $getPlayerLevel")
+}
+
+///Enumeration Class
+enum class playerID( val value: Int){
+    Sectio(15),
+    Kautsar(16),
+    Ramadhani(74)
+}
---
+++ KK4ControlFlow/src/main/kotlin/For_Loop.kt
@@ -0,0 +1,7 @@
+
+fun main() {
+    val ranges = 1..5
+    for (i in ranges) {
+        println("$i say 'im in ranges Bro!!'")
+    }
+}
